d83f3534472ad97b314bd3198fb22ab8c0a922bb
Quentin Veys <quentin_veys@hotmail.com>
2016-01-09 15:45:37 +0100

    09/01 15/45

---
+++ DolphinApp/DolphinApp/Trigger/DeviceTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace DolphinApp.Trigger
+{
+    public class DeviceTrigger : StateTriggerBase
+    {
+
+        public bool MobileDevice { get;  set; }
+
+        public DeviceTrigger()
+        {
+            SetActive(MobileDevice = (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile"));
+        }
+
+    }
+}
